cd27d0d25c299c3ce4ec57d73502ae4b94aa1f69
Qootho93 <preciousqootho@gmail.com>
2023-09-07 16:05:26 -0700

    101 quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a c program tha printz a line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\- Dora Korpar, 2015-10-19\n",stdoutput);
+	return (1);
+}
