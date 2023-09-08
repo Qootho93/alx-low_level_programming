ba8da4b0a50286e0257cbf9d0f40ca63653424ba
Qootho93 <preciousqootho@gmail.com>
2023-09-07 23:52:09 -0700

    4 puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
  /**
- *main this is main function
- *   Return : 0 (success)`
-  *\
-   int main(void)
+  *main this is main function
+  *   Return : 0 (success)`
+  */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
+
