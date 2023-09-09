cd1e21427a05143c0d3b69e490790d1488ea97d8
Qootho93 <preciousqootho@gmail.com>
2023-09-09 12:15:17 -0700

    0-positive_or_negative.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - a c program tha printz a line
- * Return: 0 (Success)
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\- Dora Korpar, 2015-10-19\n",stdoutput);
-	return (1);
+	write(2,"and that piece of art is useful\"-Dora Korpa, 2015-10-19\n",59);
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+my shell variables if,else, while
