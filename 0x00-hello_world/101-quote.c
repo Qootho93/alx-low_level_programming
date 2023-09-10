81a5828812933c8ec388c47984cdddb9fecb626d
Qootho93 <preciousqootho@gmail.com>
2023-09-10 13:33:22 -0700

    101 quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\"-Dora Korpa, 2015-10-19\n",59);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
+
