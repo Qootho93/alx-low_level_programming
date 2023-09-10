152642e6bbe3ea7bd6333b441667e2e3d3f5f6eb
Qootho93 <preciousqootho@gmail.com>
2023-09-10 01:15:43 -0700

    5 print numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print the numbers from 0 to 9
+ * Return: Always 0 (success)
+ */
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
