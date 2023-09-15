e9c078a8e2371bf628a95cffd0ed9acc3623871f
Qootho93 <preciousqootho@gmail.com>
2023-09-15 13:09:06 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,11 @@ void more_numbers(void)
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
+		{
 			if (j >= 10)
 				_putchar('1');
 		_putchar (j % 10 + '0');
-	}
+		}
 	_putchar('\n');
+	}
 }
