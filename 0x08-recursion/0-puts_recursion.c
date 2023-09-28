f4ba97874a58d9143846286f022ca72f52a2716a
Qootho93 <preciousqootho@gmail.com>
2023-09-27 23:02:57 -0700

    0 puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print the string followed by a new line
+ * @s: point to the firest char in string
+ * Return: always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
