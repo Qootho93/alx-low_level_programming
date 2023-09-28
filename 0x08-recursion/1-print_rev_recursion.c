8c09a7bfb9dda63cc1fdbcc44674d877e6367d2a
Qootho93 <preciousqootho@gmail.com>
2023-09-27 23:22:59 -0700

    1 print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: input
+ * Return: 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+	else
+	{
+		return;
+	}
+}
