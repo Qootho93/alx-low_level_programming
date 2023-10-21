37f7d474adf981c3236bf8afaa0f5f2a29a906b4
Qootho93 <preciousqootho@gmail.com>
2023-10-21 12:15:53 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,17 +10,19 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *temp;
-	unsigned int length = 0;
+	list_t *new_node;
+	list_t *temp = *head;
+	unsigned int len = 0;
 
-	while (str[length])
-		length++;
+	while (str[len])
+		len++;
 
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
+
 	new_node->str = strdup(str);
-	new_node->len = length;
+	new_node->len = len;
 	new_node->next = NULL;
 
 	if (*head == NULL)
@@ -33,5 +35,6 @@ list_t *add_node_end(list_t **head, const char *str)
 		temp = temp->next;
 
 	temp->next = new_node;
+
 	return (new_node);
 }
