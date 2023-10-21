cd7120507ad88f3cd84e72e32e63f1fe0e7bf41a
Qootho93 <preciousqootho@gmail.com>
2023-10-21 13:38:15 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,9 @@
-	global  main
+global  main
 	 extern  printf
 main:
-	mov  edi, format
-	xor  eax, eax
+	mov edi, format
+	xor eax, eax
 	call printf
 	mov	eax, 0
 	ret
-format: db `Hello, Holberton\n`,0
+format: db `Hello, Holberton\n`, 0
