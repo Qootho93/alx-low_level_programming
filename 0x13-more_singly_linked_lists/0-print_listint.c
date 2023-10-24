28df7e1f2a0503c8320a8a22befb2d309fe3adcc
Qootho93 <preciousqootho@gmail.com>
2023-10-24 14:42:24 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,6 +3,7 @@
 /**
  * print_listint -  prints all the elements of a listint_t list.
  * @h: type of listint to type
+ *
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
