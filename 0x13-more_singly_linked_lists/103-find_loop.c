37ab8774f5ce8b5b35a6f49bf037356aa5a838fd
Qootho93 <preciousqootho@gmail.com>
2023-10-24 14:17:05 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -6,7 +6,7 @@
  * Return: The address of the node where the loop starts,
  * or NULL if there is no loop
  */
-listint_t *find_listint_loop(listint_t *head);
+listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow;
 	listint_t *fast;
