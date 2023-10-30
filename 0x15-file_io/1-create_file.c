c364bb648e01d8bc7dcbcd50dd0de21e8e6978fb
Qootho93 <preciousqootho@gmail.com>
2023-10-30 13:56:10 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file to create
+ * @text_content: pointer to a string
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, q, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	q = write(fd, text_content, len);
+
+	if (fd == -1 || q == -1)
+		return (-1);
+	close(fd);
+	return (1);
+
+}
